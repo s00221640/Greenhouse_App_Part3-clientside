26e246127c93df4abb7aca2169be9fdecbee361c
Jack Tierney <123546995+s00221640@users.noreply.github.com>
2024-12-18 15:00:11 +0000

    Delete dist directory

--- dist/auth.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authenticateKey = void 0;
-const authenticateKey = (req, res, next) => {
-    //No key yet
-    console.log("Authentication middleware triggered");
-    next();
-};
-exports.authenticateKey = authenticateKey;
