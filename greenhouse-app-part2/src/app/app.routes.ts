ab301fd93c10f9f0c7eb6fbf959ac65ffb4f1d6a
Jack Tierney <s00221640@atu.ie>
2024-12-18 22:26:13 +0000

    Cleaning up routing

--- greenhouse-app-part2/src/app/app.routes.ts
+++ greenhouse-app-part2/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PlantListComponent } from './components/plant-list/plant-list.component';
+import { PlantCreateComponent } from './components/plant-create/plant-create.component';
+import { PlantEditComponent } from './components/plant-edit/plant-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PlantListComponent },
+  { path: 'create', component: PlantCreateComponent },
+  { path: 'edit/:id', component: PlantEditComponent }
+];
