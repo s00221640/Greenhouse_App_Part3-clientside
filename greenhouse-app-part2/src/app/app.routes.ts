cc824398a8483428432be59b9bbd2b75263e15c7
Jack Tierney <s00221640@atu.ie>
2025-04-10 04:08:17 +0100

    delete user

--- greenhouse-app-part2/src/app/app.routes.ts
+++ greenhouse-app-part2/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { PlantEditComponent } from './components/plant-edit/plant-edit.component
 import { AppComponent} from './app.component';
 import { LoginComponent } from './components/login.component';
 import { AlmanacComponent } from './components/almanac/almanac.component';
+import { AdminDashboardComponent } from './components/admin-dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: PlantListComponent },
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'login', component: LoginComponent },
   { path: 'almanac', component: AlmanacComponent },
+  { path: 'admin', component: AdminDashboardComponent },
 ];
 
 console.log(routes);
